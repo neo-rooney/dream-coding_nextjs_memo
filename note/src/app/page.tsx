cb9c5a988fa865d73a64f3c509528d2258b3104d
rooney <bch3454@gmail.com>
2023-03-30 11:06:35 +0900

    Static routing(12 and 13)

---
+++ note/src/app/page.tsx
@@ -0,0 +1,8 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <h1>홈</h1>;
+}
