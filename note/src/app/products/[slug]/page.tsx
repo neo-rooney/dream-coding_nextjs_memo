b6dc41716c3ad6c1ebe01e2d89dd880f96762f89
rooney <bch3454@gmail.com>
2023-03-30 16:04:54 +0900

    SSG 랜더링

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -13,17 +13,17 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
   if (!product) {
     notFound();
   }
-  return <h1>{product} 제품 설명 페이지</h1>;
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = getProducts();
+export async function generateStaticParams() {
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,15 +1,15 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-export default function ProductsPage() {
-  const products = getProducts();
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+        {products.map(({ id, name }, index) => (
+          <li key={id}>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
