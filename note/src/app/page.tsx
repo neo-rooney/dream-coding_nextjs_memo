ee7ecbda2b137cfb81677fc9a115af0523a48138
rooney <bch3454@gmail.com>
2023-03-31 09:56:31 +0900

    Images

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,10 +1,18 @@
 import Counter from "@/components/Counter";
+import Image from "next/image";
 
 export default function Home() {
   console.log("안녕 - 서버");
   return (
     <>
       <h1>홈페이지다!!</h1>
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="Images"
+        width={400}
+        height={400}
+        priority
+      />
       <Counter />
     </>
   );
